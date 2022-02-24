fa674df17dadaca086bc7e83c96a907424eac74c
I-JSPR-I <jaspermeersschaut@hotmail.com>
2022-02-24 12:07:00 +0100

    English translation

--- 22-02-22 classes ex 1/Program.cs
+++ 22-02-22 classes ex 1/Program.cs
@@ -8,30 +8,30 @@ namespace _22_02_22_classes_ex_1
     {
         static void Main(string[] args)
         {
-            Leerling gegevens;
-            List<Leerling> leerlingen = new List<Leerling>();
+            Student gegevens;
+            List<Student> Studenten = new List<Student>();
            
             do
             {
                 Console.Clear();
-                gegevens = new Leerling();
-                gegevens.Leerlingnummer++;
-                Console.WriteLine("Wat is de voornaam van de leerling?!");
-                gegevens.VoorNaamLeerling = Console.ReadLine();
+                gegevens = new Student();
+                gegevens.Studentnummer++;
+                Console.WriteLine("Wat is de FirstName van de Student?!");
+                gegevens.FirstNameStudent = Console.ReadLine();
 
-                Console.WriteLine("Wat is de achternaam van de leerling?!");
-                gegevens.AchterNaamLeerling = Console.ReadLine();
+                Console.WriteLine("Wat is de LastName van de Student?!");
+                gegevens.LastNameStudent = Console.ReadLine();
 
-                Console.WriteLine("Wat is de klas van de leerling?!");
-                gegevens.Klasleerling = Console.ReadLine();
+                Console.WriteLine("Wat is de Class van de Student?!");
+                gegevens.ClassStudent = Console.ReadLine();
 
-                leerlingen.Add(gegevens);
+                Studenten.Add(gegevens);
 
-                Console.WriteLine("Wilt u nog een leerling toevoegen? (J/N)");
+                Console.WriteLine("Wilt u nog een Student toevoegen? (J/N)");
             } while (Console.ReadLine().ToUpper().Substring(0, 1) == "J");
-            Console.WriteLine($"Welke leerling wilt u tonen? (1 t.e.m {leerlingen.Count})");
+            Console.WriteLine($"Welke Student wilt u tonen? (1 t.e.m {Studenten.Count})");
             int i = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine($"{leerlingen[i]}");
+            Console.WriteLine($"{Studenten[i]}");
 
         }
     }
