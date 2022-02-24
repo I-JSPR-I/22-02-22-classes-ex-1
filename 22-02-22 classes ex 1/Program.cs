8c4c7f598ebff5f56c6ae56dec8e392533f57a9f
I-JSPR-I <jaspermeersschaut@hotmail.com>
2022-02-24 20:11:05 +0100

    Use of methods

--- 22-02-22 classes ex 1/Program.cs
+++ 22-02-22 classes ex 1/Program.cs
@@ -7,34 +7,43 @@ namespace _22_02_22_classes_ex_1
     class Program
     {
         static void Main(string[] args)
-        {
-            Student Data;
+        { 
             List<Student> students = new List<Student>();
-           
             do
             {
-                Console.Clear();
-                Data = new Student();
-                Console.WriteLine("What is the first name of the student?");
-                Data.FirstNameStudent = Console.ReadLine();
-
-                Console.WriteLine("What is the last name of the student?");
-                Data.LastNameStudent = Console.ReadLine();
-
-                Console.WriteLine("What class is the student in?");
-                Data.ClassStudent = Console.ReadLine();
-
-                students.Add(Data);
+                input(students);
 
                 Console.WriteLine("Do you want to add another student? (Y/N)");
             } while (Console.ReadLine().ToUpper().Substring(0, 1) == "Y");
+            output(students);
+        }
+
+        private static void output(List<Student> students)
+        {
             Console.WriteLine($"Which student do you want to see? (between 1 and {students.Count})");
             int i = Convert.ToInt32(Console.ReadLine());
-            i++;
+            i--;
             Console.WriteLine($"{students[i].FirstNameStudent}");
             Console.WriteLine($"{students[i].LastNameStudent}");
             Console.WriteLine($"{students[i].ClassStudent}");
+        }
 
+        private static List<Student> input(List<Student> students)
+        {
+            Student Data;
+            Console.Clear();
+            Data = new Student();
+            Console.WriteLine("What is the first name of the student?");
+            Data.FirstNameStudent = Console.ReadLine();
+
+            Console.WriteLine("What is the last name of the student?");
+            Data.LastNameStudent = Console.ReadLine();
+
+            Console.WriteLine("What class is the student in?");
+            Data.ClassStudent = Console.ReadLine();
+
+            students.Add(Data);
+            return students;
         }
     }
 }
