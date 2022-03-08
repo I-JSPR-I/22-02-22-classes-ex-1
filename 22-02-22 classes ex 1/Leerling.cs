0e015c3b04967c07a5ea9b81823d10768ab49339
I-JSPR-I <jaspermeersschaut@hotmail.com>
2022-03-08 08:54:53 +0100

    Class update

--- 22-02-22 classes ex 1/Leerling.cs
+++ 22-02-22 classes ex 1/Leerling.cs
@@ -10,6 +10,19 @@ namespace _22_02_22_classes_ex_1
         public string LastNameStudent;
         public string ClassStudent;
 
+        public Student(){
+            FirstNameStudent = "Voornaam";
+            LastNameStudent = "Achternaam";
+            ClassStudent = "IB";
+        }
+        public Student(string FirstName, string LastName, string Class)
+        {
+
+            FirstNameStudent = FirstName;
+            LastNameStudent = LastName;
+            ClassStudent = Class;
+
+        }
     }
 }
 
